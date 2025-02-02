91d79d7fba2b4190eb81f4bc48690dddcc104552
Grigoriy <egrigoriy@gmail.com>
2025-02-02 14:20:22 +0300

    Project 4: Fill.asm program done

--- projects/4/fill/Fill.asm
+++ projects/4/fill/Fill.asm
@@ -8,4 +8,40 @@
 // i.e. writes "black" in every pixel. When no key is pressed, 
 // the screen should be cleared.
 
-//// Replace this comment with your code.
+(LOOP_KBD)
+// set default color to white
+@color
+M=0
+// if no key touch just fill with default color
+@KBD
+D=M
+@FILL
+D; JEQ
+// else change color to black
+@color
+M=-1
+// fill the screen with the color set abouve
+(FILL)
+// set addr = SCREEN
+@SCREEN
+D=A
+@addr
+M=D
+(LOOP_FILL)
+// set RAM[addr] = color
+@color
+D=M
+@addr
+A=M
+M=D
+// set addr++
+@addr
+MD=M+1
+// if addr < KBD then goto LOOP_FILL
+@KBD
+D=D-A
+@LOOP_FILL
+D; JLT
+// else loop back to key listening
+@LOOP_KBD
+0; JMP
