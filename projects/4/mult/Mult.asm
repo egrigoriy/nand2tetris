3488dd34176c8e729c6121af2b401421bf2e6c35
Grigoriy <egrigoriy@gmail.com>
2025-02-02 12:55:15 +0300

    Project 4: Mult.asm program done

--- projects/4/mult/Mult.asm
+++ projects/4/mult/Mult.asm
@@ -7,4 +7,33 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 // The algorithm is based on repetitive addition.
 
-//// Replace this comment with your code.
+// R2=0
+// if R0==0 goto END else n=R0
+// LOOP
+// R2 = R2 + R1
+// n = n - 1
+// if n > 0 goto LOOP
+// END
+
+@R2
+M=0
+@R0
+D=M
+@END
+D, JEQ
+@R0
+D=M
+@n
+M=D
+(LOOP)
+@R1
+D=M
+@R2
+M=D+M
+@n
+MD=M-1
+@LOOP
+D, JGT
+(END)
+@END
+0; JMP
